15c86158f584322e439a5c882ffcb39ce6358cc5
Fabio Henrique Antonio <fabio.henrique@confianca.com.br>
2022-09-22 13:38:06 -0300

    First commit

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,32 @@
+import { LoginModel } from './../../models/LoginModel';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        senha: ['', [Validators.required]]
+      }
+    );
+  }
+
+  submitLogin() {
+    debugger
+    var dadosLogin = this.loginForm.getRawValue() as LoginModel;
+  }
+
+}
